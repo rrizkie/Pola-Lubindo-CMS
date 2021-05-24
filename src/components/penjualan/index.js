47b0289f2b251282bc79e0320eecd68e56c65223
fbryo21 <febryryo21@gmail.com>
2021-05-24 18:46:42 +0700

    NAV TRANSAKSI

--- src/components/penjualan/index.js
+++ src/components/penjualan/index.js
@@ -18,18 +18,18 @@ import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import QueryBuilderOutlinedIcon from "@material-ui/icons/QueryBuilderOutlined";
 
 import useStyles from "./styles";
-import { CMSContext } from "../../context/state";
+// import { CMSContext } from "../../context/state";
 
 import { Link } from "react-router-dom";
 
 export default function Index() {
   const classes = useStyles();
-  const { transaksi, fetchTransaksi } = useContext(CMSContext);
-  console.log(transaksi);
+  // const { transaksi, fetchTransaksi } = useContext(CMSContext);
+  // console.log(transaksi);
 
-  useEffect(() => {
-    fetchTransaksi();
-  }, []);
+  // useEffect(() => {
+  //   fetchTransaksi();
+  // }, []);
 
   function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -247,6 +247,21 @@ export default function Index() {
   const [view, setView] = React.useState("semua produk");
   return (
     <>
+      {views.map((option) => (
+        <Button
+          key={option.value}
+          onClick={() => setView(option.value)}
+          style={{
+            borderBottom:
+              view === option.value ? "2px solid red" : "2px solid black",
+            borderRadius: 0,
+            color: view === option.value ? "red" : null,
+          }}
+          className={classes.form_daftar_btn}
+        >
+          <b>{option.value}</b>
+        </Button>
+      ))}
       <form className={classes.form_kategori} noValidate autoComplete="off">
         <TextField
           variant="outlined"
@@ -307,146 +322,7 @@ export default function Index() {
         <Button variant="outlined" className={classes.form_daftar_btn}>
           cetak label
         </Button>
-
-        {views.map((option) => (
-          <Button
-            key={option.value}
-            onClick={() => setView(option.value)}
-            style={{
-              borderBottom:
-                view === option.value ? "2px solid red" : "2px solid black",
-              borderRadius: 0,
-              color: view === option.value ? "red" : null,
-            }}
-            className={classes.form_daftar_btn}
-          >
-            <b>{option.value}</b>
-          </Button>
-        ))}
       </form>
-      {transaksi.map((item) => (
-        <Paper className={classes.paper}>
-          <Grid container spacing={3}>
-            <Grid item xs={8}>
-              <Breadcrumbs aria-label="breadcrumb">
-                <FormControlLabel
-                  control={
-                    <RedCheckbox
-                      checked={pilih.checkedA}
-                      onChange={handlePilih}
-                      name="checkedA"
-                    />
-                  }
-                  label={
-                    <Typography color="textPrimary">
-                      {item.statusPesanan}
-                    </Typography>
-                  }
-                />
-                <Link color="inherit" href="/">
-                  {item.invoice}
-                </Link>
-                <Link color="inherit" href="/getting-started/installation/">
-                  {item.namaPenerima}
-                </Link>
-                <Typography color="textPrimary">
-                  {item.createdAt.split("T")[0]} |{" "}
-                  {item.createdAt.split("T")[1].split(".")[0]}
-                </Typography>
-              </Breadcrumbs>
-            </Grid>
-            <Grid item xs={4} className={classes.grid_align_right}>
-              <BatasRespons />
-            </Grid>
-          </Grid>
-          <Grid container spacing={3} className={classes.grid_border_top}>
-            <Grid item xs={4}>
-              {item.Carts.map((produk) => (
-                <Grid container spacing={3} key={produk.Produk.id}>
-                  <Grid item xs={3}>
-                    <img
-                      src={produk.Produk.fotoProduk}
-                      alt={produk.Produk.namaProduk}
-                      width="50"
-                      height="50"
-                    />
-                  </Grid>
-                  <Grid item xs={9}>
-                    {produk.Produk.namaProduk}
-                    <br />
-                    {produk.qty} x Rp. {produk.Produk.hargaSatuan}
-                  </Grid>
-                </Grid>
-              ))}
-            </Grid>
-            <Grid item xs={5} className={classes.grid_border_left}>
-              <Typography color="textPrimary">
-                Alamat
-                <br />
-                {item.alamatPengiriman}
-              </Typography>
-            </Grid>
-            <Grid item xs={3} className={classes.grid_border_left}>
-              <Typography color="textPrimary">
-                Kurir JNE
-                <br />
-                Rp {item.ongkosKirim}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container spacing={3}>
-            <Grid item xs={9}>
-              +2 produk lain&nbsp;
-              <Link href="#" onClick={() => setLihat(!lihat)}>
-                {lihat ? "tutup" : "lihat"}
-              </Link>
-              {/* {lihat ? (
-                <Paper className={classes.paper}>
-                  {field.produk.map((sub_field) => (
-                    <Grid container spacing={3}>
-                      {sub_field.sub_produk.map((sub_produk_field) => (
-                        <>
-                          <Grid item xs={3}>
-                            <img
-                              src={sub_produk_field.gmbr_produk}
-                              alt={sub_produk_field.nm_produk}
-                              width="50"
-                              height="50"
-                            />
-                          </Grid>
-                          <Grid item xs={9}>
-                            {sub_produk_field.nm_produk}
-                            <br />
-                            {sub_produk_field.jumlah} x {sub_produk_field.harga}
-                          </Grid>
-                        </>
-                      ))}
-                    </Grid>
-                  ))}
-                </Paper>
-              ) : null} */}
-              <br />
-              Total bayar
-            </Grid>
-            <Grid item xs={3} className={classes.grid_align_right}>
-              <Typography color="textPrimary">
-                <b>Rp. {item.jumlahBayar}</b>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            spacing={3}
-            alignItems="center"
-            className={classes.grid_border_top}
-          >
-            <Grid item xs={4}>
-              {/* sales by: {field.sales_by} */}
-            </Grid>
-            <BtnPesanan />
-          </Grid>
-        </Paper>
-      ))}
     </>
   );
 }
